2bf821ca4f770af1cc8c176252eff2f7aec0350a
PrikolMen <olegis019@gmail.com>
2023-05-07 02:10:47 +0400

    Updated

--- lua/packages/dev-tools/visual-debugger.lua
+++ lua/packages/dev-tools/visual-debugger.lua
@@ -1,13 +1,45 @@
+local render = render
+local hook = hook
+local draw = draw
+local hook = hook
+local cam = cam
+
+local TEXT_ALIGN_CENTER = TEXT_ALIGN_CENTER
+local table_Empty = table.Empty
+local math_min = math.min
+local IsValid = IsValid
+local Vector = Vector
+local ipairs = ipairs
+
 local identifier = gpm.Package:GetIdentifier( "visual-debugger" )
 local developer = GetConVar( "developer" )
+local vector_zero = Vector()
 local debugObject = {
     ["IsValid"] = false
 }
 
 local distance = 4096
 
+local colors = {
+    Color( 255, 0, 0 ),
+    Color( 0, 255, 0 ),
+    Color( 0, 0, 255 )
+}
+
+local axis = {
+    "X+",
+    "Y+",
+    "Z+"
+}
+
+local angles = {
+    Angle( 0, 0, 0 ),
+    Angle( 0, 90, 0 ),
+    Angle( -90, 90, 90 )
+}
+
 function DevTools.VisualDebugger()
-    if developer:GetInt() < 4 then
+    if developer:GetInt() < 2 then
         hook.Remove( "PostDrawOpaqueRenderables", identifier )
         hook.Remove( "HUDPaint", identifier )
         hook.Remove( "Think", identifier )
@@ -33,7 +65,7 @@ function DevTools.VisualDebugger()
 
         entity = debugObject.Entity
         if not IsValid( entity ) then
-            table.Empty( debugObject )
+            table_Empty( debugObject )
             debugObject.IsValid = false
             return
         end
@@ -45,10 +77,24 @@ function DevTools.VisualDebugger()
         debugObject.Origin = entity:GetPos()
         debugObject.Angles = entity:GetAngles()
 
-        -- OBB
-        debugObject.Center = entity:OBBCenter()
-        debugObject.Mins = entity:OBBMins()
-        debugObject.Maxs = entity:OBBMaxs()
+        -- OBB mins, maxs
+        local mins, maxs = entity:OBBMins(), entity:OBBMaxs()
+        debugObject.Mins = mins
+        debugObject.Maxs = maxs
+
+        -- Lines
+        local box = ( maxs - mins )
+        local lenght = math_min( box[1], box[2], box[3] ) / 4
+        local startPos = entity:LocalToWorld( vector_zero )
+
+        local lines = {}
+        for index = 1, 3 do
+            local vec = Vector()
+            vec[ index ] = lenght
+            lines[ index ] = { startPos, entity:LocalToWorld( vec ), entity:LocalToWorldAngles( angles[ index ] ) }
+        end
+
+        debugObject.Lines = lines
 
         -- Color
         debugObject.Color = entity:GetColor()
@@ -59,6 +105,17 @@ function DevTools.VisualDebugger()
         if not debugObject.IsValid then return end
 
         render.DrawWireframeBox( debugObject.Origin, debugObject.Angles, debugObject.Mins, debugObject.Maxs, debugObject.Color, false )
+
+        for index, data in ipairs( debugObject.Lines ) do
+            local color = colors[ index ]
+            render.DrawLine( data[1], data[2], color, false )
+
+            cam.IgnoreZ( true )
+                cam.Start3D2D( data[2], data[3], 0.1 )
+                    draw.DrawText( axis[ index ], "Default", 0, 0, color, TEXT_ALIGN_CENTER )
+                cam.End3D2D()
+            cam.IgnoreZ( false )
+        end
     end )
 
     hook.Add( "HUDPaint", identifier, function()
